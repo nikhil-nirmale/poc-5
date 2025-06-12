3f27d15838b7269fc311dcb333a16160f18e58e5
nikhil-nirmale <nikhil.nirmale@5exceptions.com>
2025-06-12 14:18:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Hello from your GitHub Actions demo app! Welcome"
+
+@app.route("/healthz")
+def health():
+    return "ok"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    print(f"🚀 Starting app on port {port}")
+    app.run(host="0.0.0.0", port=port)
